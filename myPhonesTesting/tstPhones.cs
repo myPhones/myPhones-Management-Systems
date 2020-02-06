add7ee71dc899ca5057e22d266cd71e95464a67d
Parth Amritlal <P2403473@my365.dmu.ac.uk>
2020-02-06 10:32:44 +0000

    Added testing and class for phones management

---
+++ myPhonesTesting/tstPhones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myPhonesTesting
+{
+    [TestClass]
+    public class tstPhones
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsPhones AnPhones = new clsPhones();
+            Assert.IsNotNull(AnPhones);
+
+        }
+    }
+}

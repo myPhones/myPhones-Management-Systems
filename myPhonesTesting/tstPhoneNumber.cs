674187250e39eb56e1ab22d169b4269787273a8c
vishal soma <you@example.com>
2020-02-13 12:16:02 +0000

    I have run the test on all the attributes to make sure they are working perfectly fine. i have done the testing for address, phone number, job title, admin rights and full names.

---
+++ myPhonesTesting/tstPhoneNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myPhonesTesting
+{
+    [TestClass]
+    public class tstPhoneNumber
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsPhoneNumber AnPhoneNumber = new clsPhoneNumber();
+            Assert.IsNotNull(AnPhoneNumber);
+        }
+    }
+}

f390e6945420e632ca0b8f3b34b31d7109e87207
Mohamadsafwan Saleh <P2442262@my365.dmu.ac.uk>
2020-02-13 12:43:57 +0000

    Tested

---
+++ myPhonesTesting/tstOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myPhonesTesting
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void TestOrder()
+        {
+            clsOrder AnOrder = new clsOrder();
+
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}

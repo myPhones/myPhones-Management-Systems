1888e2715acfde5a9a7091b90a41396e7c38f04b
Mohamadsafwan Saleh <P2442262@my365.dmu.ac.uk>
2020-02-17 13:28:09 +0000

    Testing for phone order table was carried out successfully

---
+++ myPhonesTesting/tstOrderID.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace myPhonesTesting
+{
+    [TestClass]
+    public class tstOrderID
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
